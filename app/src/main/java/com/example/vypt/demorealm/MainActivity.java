6af5ecb2705c03d2b42699e04f67abff29cc0b6c
TheVy <ptvy3197@gmail.com>
2018-05-21 17:05:27 +0700

    add method add coutry

--- app/src/main/java/com/example/vypt/demorealm/MainActivity.java
+++ app/src/main/java/com/example/vypt/demorealm/MainActivity.java
@@ -4,23 +4,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
 public class MainActivity extends AppCompatActivity {
     Realm myRealm;
-    Button b1, b2, b3;
+    Button b_view, b_fil, b_order, b_add;
     TextView tv;
+    EditText et_name, et_pop;
+    int x = 4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b1 = (Button) findViewById(R.id.b_1);
-        b2 = (Button) findViewById(R.id.b_2);
-        b3 = (Button) findViewById(R.id.b_3);
+        b_add = (Button) findViewById(R.id.b_add);
+        b_fil = (Button) findViewById(R.id.b_fil);
+        b_order = (Button) findViewById(R.id.b_order);
+        b_view = (Button) findViewById(R.id.b_view);
         tv = (TextView) findViewById(R.id.tv);
+        et_name = (EditText) findViewById(R.id.et_name);
+        et_pop = (EditText) findViewById(R.id.et_pop);
 
         myRealm = Realm.getInstance(this);
         // cach 1
@@ -28,31 +35,46 @@ public class MainActivity extends AppCompatActivity {
 
         Country country1 = myRealm.createObject(Country.class);
         country1.setName("Viet Nam");
-        country1.setCode("1001");
+        country1.setCode("1");
         country1.setPopulation(10000000);
 
         Country country3 = myRealm.createObject(Country.class);
         country3.setName("TQ");
-        country3.setCode("1003");
+        country3.setCode("2");
         country3.setPopulation(100000000);
 
         Country country4 = myRealm.createObject(Country.class);
         country4.setName("Canada");
-        country4.setCode("1004");
+        country4.setCode("3");
         country4.setPopulation(30000000);
 
         myRealm.commitTransaction();
         //cach 2
         Country country2 = new Country();
         country2.setName("USA");
-        country2.setCode("1002");
+        country2.setCode("4");
         country2.setPopulation(50000000);
 
         myRealm.beginTransaction();
         Country copyofCountry2 = myRealm.copyToRealm(country2);
         myRealm.commitTransaction();
 
-        b1.setOnClickListener(new View.OnClickListener() {
+        b_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                x = x+1;
+                myRealm.beginTransaction();
+                Country country = myRealm.createObject(Country.class);
+                country.setName(et_name.getText().toString());
+                country.setCode(String.valueOf(x));
+                country.setPopulation(Integer.valueOf(et_pop.getText().toString()));
+                myRealm.commitTransaction();
+                Toast.makeText(MainActivity.this, "Add successful",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        b_view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 RealmResults<Country> results1 =
@@ -61,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        b2.setOnClickListener(new View.OnClickListener() {
+        b_fil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 RealmResults<Country> results2 =
@@ -70,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        b3.setOnClickListener(new View.OnClickListener() {
+        b_order.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 RealmResults<Country> results3 =
